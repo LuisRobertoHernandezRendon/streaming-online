a2b4376d97524e920d46eb9c7402374a2df18ee4
Luis Hernandez <luis.hernandezrendon@quimica.unam.mx>
2025-07-04 11:18:21 -0600

    se agrega logica para responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,28 @@
-import { Link } from "react-router-dom";
+// src/components/Header.jsx
+import { useState } from "react";
+import NavBar from "./NavBar";
 import logo from "../assets/logo.png";
 import "../styles/header.css";
 
 export default function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen((prev) => !prev);
+  };
+
   return (
     <header className="header">
       <div className="header__logo">
         <img src={logo} alt="Streamwave Logo" className="header__logo-image" />
         <h1 className="header__title">Streamwave</h1>
       </div>
-      <nav className="header__nav">
-        <Link className="header__nav-item" to="/">
-          Inicio
-        </Link>
-        <Link className="header__nav-item" to="/rentals">
-          Mis Alquileres
-        </Link>
-        <Link className="header__nav-item" to="/purchases">
-          Compras
-        </Link>
-      </nav>
+
+      <button className="header__toggle" onClick={toggleMenu}>
+        ☰
+      </button>
+
+      <NavBar isOpen={menuOpen} toggleMenu={toggleMenu} />
     </header>
   );
 }
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,19 @@
+// src/components/NavBar.jsx
+import { Link } from "react-router-dom";
+import "../styles/navbar.css";
+
+export default function NavBar({ isOpen, toggleMenu }) {
+  return (
+    <nav className={`nav ${isOpen ? "nav--open" : ""}`}>
+      <Link className="nav__item" to="/" onClick={toggleMenu}>
+        Inicio
+      </Link>
+      <Link className="nav__item" to="/rentals" onClick={toggleMenu}>
+        Mis Alquileres
+      </Link>
+      <Link className="nav__item" to="/purchases" onClick={toggleMenu}>
+        Compras
+      </Link>
+    </nav>
+  );
+}
